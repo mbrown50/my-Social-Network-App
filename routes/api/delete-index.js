765790ffabd0f1772b7565a6948953a49bb229e4
M <melissabrown@Melissas-MBP.paulbunyan.net>
2024-04-03 02:46:20 -0500

    delete extra files

--- routes/api/delete-index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const courseRoutes = require('./delete-courseRoutes');
-const studentRoutes = require('./delete-studentRoutes');
-
-router.use('/courses', courseRoutes);
-router.use('/students', studentRoutes);
-
-module.exports = router;
